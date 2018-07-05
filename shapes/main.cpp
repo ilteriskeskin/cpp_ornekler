fa7777d8b678e9cce2f625ce947b64d59213ea64
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2018-07-05 20:30:48 +0300

    Kare-Dikdörtgen çizdiren program eklendi (shapes)

---
+++ shapes/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void draw(int, int);
+
+int main(int argc, char const *argv[])
+{
+
+    int kenar1, kenar2;
+
+    cout << "1. Kenari giriniz: ";
+    cin >> kenar1;
+    cout << "2. Kenari giriniz: ";
+    cin >> kenar2;
+
+    draw(kenar1, kenar2);
+
+    cout << endl;
+
+    return 0;
+}
+
+void draw(int kenar1, int kenar2)
+{
+
+    for(int i = 1; i <= kenar1; i++)
+        cout << "*";
+    cout << endl;
+
+    for(int j = 1; j < kenar2; j++)
+        cout << "*" << string(kenar1 - 2, ' ') << "*" << endl;
+
+    for(int k = 1; k <= kenar1; k++)
+        cout << "*";
+    cout << endl;
+}
